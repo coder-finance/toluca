ed8376880ba946d278fc8e8a70e77ccb561688e4
Oleg Kubrakov <oleg.kubrakov@proton.me>
2022-07-23 14:34:14 -0700

    Tests pass for upgradebale governor.

--- contracts/test/CoderDAO.js
+++ contracts/test/CoderDAO.js
@@ -1,5 +1,5 @@
 const { ethers, upgrades } = require("hardhat");
-const { expect } = require("chai");
+const assert = require('assert');
 
 describe("CoderDAO", function () {
   const name = 'CoderDAO';
@@ -7,16 +7,21 @@ describe("CoderDAO", function () {
   before('get factories', async function () {
     this.dao = await ethers.getContractFactory("CoderDAO");
     this.token = await ethers.getContractFactory("CoderDAOToken");
+    this.daoV2 = await ethers.getContractFactory("CoderDAOV2");
   })
 
   describe("Deployment", function () {
     it("Should set the right name", async function () {
-      const token_instance = await upgrades.deployProxy(this.token);
-      await token_instance.deployed();
+      const token_instance = await upgrades.deployProxy(this.token, {kind: 'uups'});
       const instance = await upgrades.deployProxy(this.dao, [token_instance.address]);
-      await instance.deployed();
-
-      expect(await instance.name()).to.be.equal(name);
+      assert(await instance.name() === name);
+    });
+    
+    it("Should be able to upgrade", async function () {
+      const token_instance = await upgrades.deployProxy(this.token, {kind: 'uups'});
+      const instance = await upgrades.deployProxy(this.dao, [token_instance.address]);
+      const daoV2 = await upgrades.upgradeProxy(instance, this.daoV2);
+      assert(await daoV2.version() === '2');
     });
   });
 });
