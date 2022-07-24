df1b5b1883e608243b78535e370f6577b359a718
Anthony Lam <anthony@blueplume.org>
2022-07-25 02:40:45 +0800

    housekeeping

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,25 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* global upgrades */
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-  const DAOToken = await hre.ethers.getContractFactory("CoderDAOToken");
-  const DAO = await hre.ethers.getContractFactory("CoderDAO");
+  const DAOToken = await hre.ethers.getContractFactory('CoderDAOToken');
+  const DAO = await hre.ethers.getContractFactory('CoderDAO');
 
-  const daoTokenInstance = await upgrades.deployProxy(DAOToken, {kind: 'uups', initializer: 'initialize' });
+  const daoTokenInstance = await upgrades.deployProxy(DAOToken, { kind: 'uups', initializer: 'initialize' });
   await daoTokenInstance.deployed();
 
-  console.log("DAOToken deployed to:", daoTokenInstance.address);
+  console.log('DAOToken deployed to:', daoTokenInstance.address);
   console.log('Symbol: ', await daoTokenInstance.symbol());
 
-  const daoInstance = await upgrades.deployProxy(DAO, [daoTokenInstance.address], {kind: 'uups', initializer: 'initialize'});
+  const daoInstance = await upgrades.deployProxy(DAO, [daoTokenInstance.address], { kind: 'uups', initializer: 'initialize' });
   await daoInstance.deployed();
 
-  console.log("DAO deployed to:", daoInstance.address);
+  console.log('DAO deployed to:', daoInstance.address);
   console.log('Name: ', await daoInstance.name());
 }
 
