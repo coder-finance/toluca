321e57023581c2a10f2aeff381572cdb9728e56d
Oleg Kubrakov <oleg.kubrakov@proton.me>
2022-08-01 22:07:29 -0700

    Added tests on voting. Not able to vote for yet.

--- contracts/test/CoderDAO.js
+++ contracts/test/CoderDAO.js
@@ -14,20 +14,28 @@ describe("CoderDAO", function () {
 
   describe("Deployment", function () {
     it("Should set the right name", async function () {
-      const [ owner, proposer, voter1, voter2, voter3, voter4, teamAddress ] = await ethers.getSigners();
-      const empty = web3.utils.toChecksumAddress(web3.utils.randomHex(20));
-      
+      const [ owner, proposer, voter1, voter2, voter3, voter4, team ] = await ethers.getSigners();
+          
       const token_instance = await upgrades.deployProxy(this.token, {kind: 'uups'});
       const instance = await upgrades.deployProxy(this.dao, [token_instance.address]);
       assert(await instance.name() === name);
       
+
+      const value = web3.utils.toWei('1');
+      await web3.eth.sendTransaction({ from: owner.address, to: instance.address, value });
+      
       const helper = new GovernorHelper(instance);
       const tokenSupply = web3.utils.toWei('100');
-      await token_instance.mint(owner, tokenSupply);
-      await helper.delegate({ token: token_instance, to: voter1, value: web3.utils.toWei('10') }, { from: owner });
-      await helper.delegate({ token: token_instance, to: voter2, value: web3.utils.toWei('7') }, { from: owner });
-      await helper.delegate({ token: token_instance, to: voter3, value: web3.utils.toWei('5') }, { from: owner });
-      await helper.delegate({ token: token_instance, to: voter4, value: web3.utils.toWei('2') }, { from: owner });
+
+      await token_instance.mint(owner.address, tokenSupply);
+      const startBalance = await team.getBalance();
+      assert.equal(startBalance.toString(), '10000000000000000000000');
+
+      await token_instance.connect(voter1).delegate(voter1.address);
+
+      //await helper.delegate({ token: token_instance, to: voter2.address, value: web3.utils.toWei('7') }, { from: owner.address });
+      //await helper.delegate({ token: token_instance, to: voter3.address, value: web3.utils.toWei('5') }, { from: owner.address });
+      //await helper.delegate({ token: token_instance, to: voter4.address, value: web3.utils.toWei('2') }, { from: owner.address });
       
       /*const proposal = helper.setProposal([
               {
@@ -37,107 +45,54 @@ describe("CoderDAO", function () {
                 },
             ], '<proposal description>');*/
   
-      const grantAmount = web3.utils.toWei('50');
-      const transferCalldata = token.interface.encodeFunctionData('transfer', [teamAddress, grantAmount]);
+      const grantAmount = web3.utils.toWei('1');
+      const transferCalldata = token_instance.interface.encodeFunctionData('transfer', [team.address, grantAmount]);
       
-      await governor.propose(
+      const proposalTx = await instance.propose(
           [token_instance.address],
           [0],
           [transferCalldata],
           "Proposal #1: Give grant to team",
         );
-             
+
+      const proposalReceipt = await proposalTx.wait(1);
+      const proposalId = proposalReceipt.events[0].args.proposalId;
+      console.log(202, proposalReceipt.events);
+      let proposalState = await instance.state(proposalId);
+      assert.equal(proposalState, '0');
+      
       // Testing voting
-      assert(await instance.hasVoted(proposal.id, owner) === false);
-      assert(await instance.hasVoted(proposal.id, voter1) === false);
-      assert(await instance.hasVoted(proposal.id, voter2) === false);
-      assert(await web3.eth.getBalance(instance.address) === value);
-      assert(await web3.eth.getBalance(teamAddress) === '0');
+      assert(await instance.hasVoted(proposalId, owner.address) === false);
+      assert(await instance.hasVoted(proposalId, voter1.address) === false);
+      //assert(await instance.hasVoted(proposal.id, voter2.address) === false);
+      console.log(101, instance.address, owner.address, team.address);
+      
+      assert.equal(await web3.eth.getBalance(instance.address), value);
+      console.log(102)
+      assert.equal((await team.getBalance()).toString(), startBalance.toString());
 
+      let voteReceipt1 = await instance.connect(voter1).castVote(proposalId, 1);
+      console.log(202, voteReceipt1.events);
+      const votes = await instance.proposalVotes(proposalId);
+      assert(votes.forVotes == '1', "Vote count mismatch");
+      
       // Run proposal
-      const txPropose = await this.helper.propose({ from: proposer });
-
-    expectEvent(
-      txPropose,
-      'ProposalCreated',
-      {
-        proposalId: this.proposal.id,
-        proposer,
-        targets: this.proposal.targets,
-        // values: this.proposal.values,
-        signatures: this.proposal.signatures,
-        calldatas: this.proposal.data,
-        startBlock: new BN(txPropose.receipt.blockNumber).add(votingDelay),
-        endBlock: new BN(txPropose.receipt.blockNumber).add(votingDelay).add(votingPeriod),
-        description: this.proposal.description,
-      },
-    );
-
-    await this.helper.waitForSnapshot();
-
-    expectEvent(
-      await this.helper.vote({ support: Enums.VoteType.For, reason: 'This is nice' }, { from: voter1 }),
-      'VoteCast',
-      {
-        voter: voter1,
-        support: Enums.VoteType.For,
-        reason: 'This is nice',
-        weight: web3.utils.toWei('10'),
-      },
-    );
-
-    expectEvent(
-      await this.helper.vote({ support: Enums.VoteType.For }, { from: voter2 }),
-      'VoteCast',
-      {
-        voter: voter2,
-        support: Enums.VoteType.For,
-        weight: web3.utils.toWei('7'),
-      },
-    );
-
-    expectEvent(
-      await this.helper.vote({ support: Enums.VoteType.Against }, { from: voter3 }),
-      'VoteCast',
-      {
-        voter: voter3,
-        support: Enums.VoteType.Against,
-        weight: web3.utils.toWei('5'),
-      },
-    );
-
-    expectEvent(
-      await this.helper.vote({ support: Enums.VoteType.Abstain }, { from: voter4 }),
-      'VoteCast',
-      {
-        voter: voter4,
-        support: Enums.VoteType.Abstain,
-        weight: web3.utils.toWei('2'),
-      },
-    );
-
-    await this.helper.waitForDeadline();
-
-    const txExecute = await this.helper.execute();
-
-    expectEvent(
-      txExecute,
-      'ProposalExecuted',
-      { proposalId: this.proposal.id },
-    );
-
-    await expectEvent.inTransaction(
-      txExecute.tx,
-      this.receiver,
-      'MockFunctionCalled',
-    );
-
-    // After
-    expect(await this.mock.hasVoted(this.proposal.id, owner)).to.be.equal(false);
-    expect(await this.mock.hasVoted(this.proposal.id, voter1)).to.be.equal(true);
-    expect(await this.mock.hasVoted(this.proposal.id, voter2)).to.be.equal(true);
-    expect(await web3.eth.getBalance(this.mock.address)).to.be.bignumber.equal('0');
-    expect(await web3.eth.getBalance(this.receiver.address)).to.be.bignumber.equal(value); 
+      /*await instance.queue(
+          [token_instance.address],
+          [0],
+          [transferCalldata],
+          descriptionHash,
+        );*/
+      
+      await instance.execute(
+          [token_instance.address],
+          [0],
+          [transferCalldata],
+          "Proposal #1: Give grant to team",
+        );
+      
+      assert.equal(await web3.eth.getBalance(instance.address).toString(), '0');
+      assert.equal((await team.getBalance()).toString(), value.add(startBalance).toString()); 
     
     });
     
